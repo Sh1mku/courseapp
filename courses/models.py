d736891c6c2413b8df14b2379fb63d810afb1685
Keremcan <keremcanadanur@gmail.com>
2023-07-10 04:36:58 +0300

    4.7 btk

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Course(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    imageUrl = models.CharField(max_length=50, blank=True)
+    date= models.DateField()
+    isActive = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.title}  {self.date}"
