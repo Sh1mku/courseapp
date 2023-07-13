bc948ae9d322689945dfbae9e5e62c8ed3b34334
Keremcan <keremcanadanur@gmail.com>
2023-07-13 03:07:32 +0300

    user login

--- courses/models.py
+++ courses/models.py
@@ -1,24 +1,37 @@
 from django.db import models
 from django.utils.text import slugify
 # Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(default="", null=False, unique=True, db_index=True, max_length=50)
+
+    # def save(self, *args, **kwargs):
+    #         self.slug = slugify(self.name)
+    #         super().save(args,kwargs)
+
+    def __str__(self):
+        return f"{self.name}"
+    
 class Course(models.Model):
     title = models.CharField(max_length=50)
     description = models.TextField()
-    imageUrl = models.CharField(max_length=50, blank=True)
-    date= models.DateField()
-    isActive = models.BooleanField()
+    imageUrl = models.CharField(max_length=50, blank=False)
+    date= models.DateField(auto_now=True)
+    isActive = models.BooleanField(default=False)
+    isHome = models.BooleanField(default=False)
     slug = models.SlugField(default="", blank=True, null=False, unique=True, db_index=True)
+    # category = models.ForeignKey("Category", default=1, on_delete=models.CASCADE, related_name="kurslar")
+    categories = models.ManyToManyField(Category)
 
-    def save(self, *args, **kwargs):
-            self.slug = slugify(self.title)
-            super().save(args,kwargs)
+
+    # def save(self, *args, **kwargs):
+    #         self.slug = slugify(self.title)
+    #         super().save(args,kwargs)
 
     def __str__(self):
-        return f"{self.title}  {self.date}"
+        return f"{self.title}"
     
-class Category(models.Model):
-    name = models.CharField(max_length=40)
-    slug = models.CharField(max_length=50)
 
-    def __str__(self):
-        return f"{self.name} {self.slug}"
+    
+    
--- courses/views.py
+++ courses/views.py
@@ -3,6 +3,9 @@ from django.shortcuts import get_object_or_404, redirect, render
 from django.http import Http404, HttpResponse, HttpResponseNotFound, HttpResponseRedirect
 from django.urls import reverse
 from .models import Course, Category
+from django.core.paginator import Paginator
+from .forms import *
+
 data ={
     "programlama":"programlama kategorisindeki kurs listesi",
     "web-gelistirme":"web-gelistirme kategorisindeki kurs listesi",
@@ -51,7 +54,7 @@ db = {
 
 def index(request):
     
-    kurslar = Course.objects.filter(isActive=1)
+    kurslar = Course.objects.filter(isActive=1,isHome=1)
     kategoriler = Category.objects.all()
 
     return render(request, 'courses/index.html',{
@@ -59,6 +62,91 @@ def index(request):
         'courses': kurslar
     })
 
+def create_course(request):
+    if request.method == "POST":
+        form=CourseCreateForm(request.POST)
+
+        if form.is_valid():
+            form.save()
+            return redirect("/kurslar")
+    else:
+        form = CourseCreateForm()
+
+    return render(request, 'courses/create-course.html',{"form" :form})
+    # alternate way to use post requests
+    
+    #     title = request.POST["title"]
+    #     description = request.POST["description"]
+    #     imageUrl = request.POST["imageUrl"]
+    #     slug = request.POST["slug"]
+    #     isActive = request.POST.get("isActive", False)
+    #     isHome = request.POST.get("isHome", False)
+
+    #     if isActive == "on":
+    #         isActive = True
+
+    #     if isHome == "on":
+    #         isHome = True
+
+    #     error=False
+    #     msg = ""
+
+    #     if title == "":
+    #         error = True
+    #         msg+= "title alani boş birakilamaz "
+            
+    #     if len(title) < 5:
+    #         error = True
+    #         msg+= "title alani en az 5 karakter olmalidir  "
+ 
+    #     if error:
+    #         return render(request, 'courses/create-course.html', {"error": True, "msg": msg})
+
+        # kurs = Course(title=title, description=description, imageUrl=imageUrl, slug=slug, isActive=isActive, isHome=isHome)
+        # kurs.save()
+        # return redirect("/kurslar")
+    
+
+def course_list(request):
+    kurslar = Course.objects.all()
+    return render(request, 'courses/course-list.html', {
+        'courses':kurslar
+    })
+
+def course_edit(request,id):
+    course = get_object_or_404(Course, pk=id)
+
+    if request.method == "POST":
+        form = CourseEditForm(request.POST, instance=course)
+        form.save()
+        return redirect("course_list")
+    else:
+     form = CourseEditForm(instance=course)
+    
+    return render(request, "courses/edit-course.html",{"form":form})
+
+def course_delete(request,id):
+    course = get_object_or_404(Course,pk=id)
+
+    if request.method=="POST":
+        course.delete()
+        return redirect("course_list")
+    return render(request,"courses/course-delete.html", {"course":course })
+
+def search(request):
+    if "q" in request.GET and request.GET["q"] !=  "":
+    
+        q = request.GET["q"]
+        kurslar = Course.objects.filter(isActive=True,title__contains=q).order_by("date")
+        # print(kurslar)
+        kategoriler = Category.objects.all()
+    else:
+        return redirect('/kurslar')
+
+    return render(request, 'courses/search.html',{
+        'categories': kategoriler,
+        'courses': kurslar,
+    })  
 
 def details(request, slug):
     # try:
@@ -73,23 +161,39 @@ def details(request, slug):
     return  render(request, 'courses/details.html', context)
 
 
-def getCoursesByCategory(request, category_name):
-    try:
-        category_text = data[category_name] 
-        return render(request, 'courses/kurslar.html',{
-            'category': category_name,'category_text': category_text})
-    except:
-        return HttpResponseNotFound("yanlis kategori secimi")
+def getCoursesByCategory(request, slug):
+    kurslar = Course.objects.filter(categories__slug=slug,isActive=True).order_by('date')
+    kategoriler = Category.objects.all()
+
+    paginator = Paginator(kurslar, 3)
+    page = request.GET.get('page',1)
+    page_obj = paginator.get_page(page)
+
+    print(page_obj.paginator.count)
+    print(page_obj.paginator.num_pages)
+
+    return render(request, 'courses/list.html',{
+        'categories': kategoriler,
+        'page_obj': page_obj,
+        'seciliKategori': slug
+    })   
+    
+    # try:
+    #     category_text = data[category_name] 
+    #     return render(request, 'courses/kurslar.html',{
+    #         'category': category_name,'category_text': category_text})
+    # except:
+    #     return HttpResponseNotFound("yanlis kategori secimi")
 
     
 
 
-def getCoursesById(request, category_id):
-    category_list = list(data.keys())
-    if(category_id > len(category_list) or category_id < 1):
-        return HttpResponseNotFound("yanlis kategori secimi")
+# def getCoursesById(request, category_id):
+#     category_list = list(data.keys())
+#     if(category_id > len(category_list) or category_id < 1):
+#         return HttpResponseNotFound("yanlis kategori secimi")
     
-    category_name = category_list[category_id-1]
-    redirect_url = reverse("courses_by_category", args=[category_name])
+#     category_name = category_list[category_id-1]
+#     redirect_url = reverse("courses_by_category", args=[category_name])
 
-    return redirect(redirect_url)
+#     return redirect(redirect_url)
